19c205e994f4d90feea0e043f7e9a1f4dca59d3b
ThanhNhaUIT <22520994@gm.uit.edu.vn>
2024-12-15 17:41:32 +0700

    Cấu trúc lại folder, chuyển sang sqllite3

--- myWeb/myWeb/settings.py
+++ myWeb/myWeb/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'myWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "websiteNhaThuoc",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
+# DATABASES = {
+#      "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "websiteNhaThuoc",
+#         "USER": "root",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
         
+#     }
+# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
